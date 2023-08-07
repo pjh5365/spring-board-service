9961392a660379c87dc378b051c9fca0c70cff57
pjh5365 <pjh5365@naver.com>
2023-08-07 14:22:40 +0900

    Feat: "스프링부트 프로젝트 시작"
    스프링부트 프로젝트 시작
    - JAVA 11
    - Spring boot 2.7.14
    - spring web
    - lombok
    - mariadb
    - thymeleaf
    
    issue: #3

---
+++ src/main/java/pjh5365/springboardservice/SpringBoardServiceApplication.java
@@ -0,0 +1,13 @@
+package pjh5365.springboardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoardServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pjh5365/springboardservice/SpringBoardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pjh5365.springboardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoardServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
