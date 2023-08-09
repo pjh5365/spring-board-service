c84bccabaaae41fd5370d9f564553d6fb83af428
pjh5365 <pjh5365@naver.com>
2023-08-09 19:48:10 +0900

    Feat: 회원가입 API 테스트 및 구현
    - 회원가입을 위해 MemberRepository 에 memberId 로 검색하는 메소드 추가
    - API 호출 시 사용될 Controller 구현
    - Controller 에서 호출할 Service 구현
    - MemberService 테스트
    
    Resolves: #10

---
+++ src/test/java/pjh5365/springboardservice/service/MemberServiceTest.java
@@ -0,0 +1,35 @@
+package pjh5365.springboardservice.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import pjh5365.springboardservice.entity.Member;
+
+import java.time.LocalDateTime;
+
+@DisplayName("회원가입 API 테스트")
+@SpringBootTest
+class MemberServiceTest {
+
+    private final MemberService memberService;
+
+    @Autowired
+    MemberServiceTest(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @DisplayName("회원 중복 확인 및 회원가입")
+    @Test
+    void registerTest() {
+        Member member = new Member();
+        member.setMemberId("testID");
+        member.setName("TestName");
+        member.setCreatedAt(LocalDateTime.now());
+        member.setEmail("TestEmail");
+        member.setPassword("PW");
+
+        memberService.register(member);
+        memberService.register(member);
+    }
+}
