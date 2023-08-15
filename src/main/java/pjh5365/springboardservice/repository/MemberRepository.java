0033e929f2a3edc329c486a5243b8a18213308e2
pjh5365 <pjh5365@naver.com>
2023-08-15 21:34:06 +0900

    Refactor: 쿼리메소드 수정 및 추가
    스프링 시큐리티 사용을 위해 쿼리 메소드 반환값 수정 및 추가
    
    Related to: #19

--- src/main/java/pjh5365/springboardservice/repository/MemberRepository.java
+++ src/main/java/pjh5365/springboardservice/repository/MemberRepository.java
@@ -3,9 +3,11 @@ package pjh5365.springboardservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pjh5365.springboardservice.entity.Member;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    List<Member> findByMemberId(String memberId);
+    Optional<Member> findByMemberId(String memberId);
+    Optional<Member> findByEmail(String email);
+
 }
