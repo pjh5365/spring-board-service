a8f0f7dd93f572661612f83823e68f5de3d28651
pjh5365 <pjh5365@naver.com>
2023-10-05 19:10:36 +0900

    Refacotr: 사용하지 않는 메서드 삭제
    연관관계 추가 및 영속성 전이로 댓글을 수동으로 삭제하지 않아도 게시글이 삭제되면 연관된 댓글이 자동으로 삭제되므로 불필요한 코드를 삭제하였다.
    
    Related to: #26

--- src/main/java/pjh5365/springboardservice/repository/CommentRepository.java
+++ src/main/java/pjh5365/springboardservice/repository/CommentRepository.java
@@ -10,9 +10,4 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findAllByPostId(Long postId);
 
-    /* TODO
-    *   트랜잭션 어노테이션이 없으면 에러 발생하던데 스프링 JPA 에 대한 공부 필요
-    *   연관관계 생각해서 미리 설정해놓으면 트랜잭션 필요 없을듯? */
-    @Transactional
-    void deleteAllByPostId(Long postId);
 }
